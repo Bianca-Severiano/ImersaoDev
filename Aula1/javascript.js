c6e4c791923ea0861cbb5123fbc2f1cb24c84ad5
Bibis <severianobianca@hotmail.com>
2023-01-31 21:49:12 -0300

    Calculadora simples de conversão Dol - Real
    Conversão de valor fixo em Dólar para real, utilizando método para arredondamento de valor decimal

--- Aula1/javascript.js
+++ Aula1/javascript.js
@@ -1,2 +1,23 @@
 /*Primeiro comando - Apresentar mensagem na tela */
 alert('Vamos iniciar nossa imersão agora!')
+
+//Projeto da Aula 1 - Conversor de moedas
+
+/*1- Criar uma variável, nomeá-la e atribuir um valor
+        Uma para o valor em dólar e a outra para a cotação
+ 2 - Armazenar o resultado da razão entre as duas variáveis em uma terceira chamada 
+ ValorEm Real - Apresentar o resultado*/
+
+var ValorEmDolar = 30;
+var CotacaoDolar = 5.32;
+var ValorEmReal = ValorEmDolar * CotacaoDolar;
+
+/*Arredondamento de valores (Duas casas decimais) - Mozzila Javascript
+
+ - Função Math.round() - Arredonda valor para inteiro mais próximo
+ - toFixedd() - Formata um núdero utilizando notação de ponto fixo
+
+    Atribuir resultado a mesma variável já criada anteriormente*/
+
+ValorEmReal = ValorEmReal.toFixed(2);
+alert ('R$'+ ValorEmReal);
