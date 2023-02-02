2c9d8bde4a9ad44950d60f7799fe028be853f137
Bibis <severianobianca@hotmail.com>
2023-02-01 21:43:46 -0300

    Finalizada aula 2

--- Aula 2/javascript.js
+++ Aula 2/javascript.js
@@ -1,17 +1,25 @@
-/* Jogo - Usuário irá chutar números até que acerte um pré determinado
+/* Jogo - Usuário irá chutar números até que acerte um pré determinado pelo método Sort()
  1. A cada chute, deverá ser dito se número é maior ou menor que o valor pré determinado */
- var NumSecreto = 581;
 
- // Solicitar ao usuário que digite algo
- var NumDigitado = prompt('Digite seu palpite:');
+ var NumSecreto = parseInt(Math.random() * 1001);
 
- //Estrutura de decisão que compara valor digitado ao secreto e com base nisso dar um resultado
- if (NumSecreto == NumDigitado) {
-    alert('Parabens! Você acertou!!');
- } else {
-    if (NumSecreto > NumDigitado) {
-        alert ('Número menor que o esperado');
-    } else {
-        alert ('Número maior que o esperado');
+/*parse = analise 
+int = inteiro
+Math.random = Escolhe número aleatório entre 0 e 0,9999 */
+
+ while (NumSecreto != NumDigitado) { // Enquanto usuário não acertar palpite
+    
+    // Solicitar ao usuário que digite algo
+    var NumDigitado = prompt('Digite seu palpite:');
+    //Estrutura de decisão que compara valor digitado ao secreto e com base nisso dar um resultado
+    if (NumSecreto == NumDigitado) { //Valor digitado é o correto
+        alert('Parabéns! Você acertou!!');
+    } else {  // Valor digitado é incorreto
+        if (NumSecreto > NumDigitado) {  // Se número < secreto
+            alert ('Número menor que o esperado');
+        } else {  // Se número > secreto
+            alert ('Número maior que o esperado');
+        };
     };
  };
+ 
