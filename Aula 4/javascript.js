20a2470c65cf48271a64e555e993682bd13ed861
Bibis <severianobianca@hotmail.com>
2023-02-03 20:22:51 -0300

    Aula 4 - imersão dev
    Em desenvolvimento

---
+++ Aula 4/javascript.js
@@ -0,0 +1,10 @@
+/*Criando nossa primeira função. Todas elas devem conter um nome próprio*/
+function adicionarFilme() {
+    // TESTE - alert('Botão funcionando');
+    var filmeFav = document.getElementById('filme').value // Pegar conteúdo pelo id da tag
+    // TESTE - alert(filmeFav): Verficar se conteúdo do input está sendo copiado
+
+    //document.write('<img src=' + filmeFav + '>') - Inserindo assim perde-se a confguração pois não determinamos o lugar do HTML onde imagem será "impressa"
+
+    
+}
